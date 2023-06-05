0696bea90c1d8a47db7b6140c05d7df109c649d3
dimon.su <syq.dev@outlook.com>
2023-06-05 12:55:46 +0800

    feat: add ubuntu bionic basic image

--- ubuntu_bionic_py39/build.sh
+++ ubuntu_bionic_py39/build.sh
@@ -5,11 +5,8 @@ echo "deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted univ
 echo "deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
 
-# active apt
-apt update & apt upgrade
-
-# sovle issue public key
-apt install gnupg -y && apt install libgomp1 -y && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA6932366A755776
+# active apt and sovle issue public key
+apt-get update && apt-get install -y gnupg && apt-get install -y libgomp1 && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA6932366A755776
 
 # 添加python源
 if [ -e "/etc/apt/sources.list.d/deadsnakes-ubuntu-ppa-bionic.list" ]; then
@@ -20,15 +17,16 @@ echo "Acquire::AllowInsecureRepositories \"true\";" >> /etc/apt/apt.conf.d/70deb
 
 # solve issue "Certificate verification failed: The certificate is NOT trusted. The certificate issuer is unknown."
 touch /etc/apt/apt.conf.d/99verify-peer.conf && echo >>/etc/apt/apt.conf.d/99verify-peer.conf "Acquire { https::Verify-Peer false }"
+apt-get update
 
-# active apt
-apt update & apt upgrade
-
-# install python
-apt install software-properties-common -y && add-apt-repository ppa:deadsnakes/ppa -y && apt update && apt install python3.9 -y && apt install python3.9-dev -y && apt install python3.9-distutils -y
-
-# install pip
-apt install curl -y && curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3.9 get-pip.py
+# install python3.9
+apt-get install software-properties-common -y && add-apt-repository ppa:deadsnakes/ppa -y && apt-get update
+apt-get install python3.9 -y
+apt-get install libc6=2.27-3ubuntu1.5 -y --allow-downgrades && apt-get install python3.9-dev -y
+apt-get install python3.9-distutils -y
+apt-get install curl -y && curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3.9 get-pip.py
+ln -s /usr/bin/python3.9 /usr/bin/python
 
 # install tool
-apt install -y vim
+apt-get install -y vim
+apt-get install -y net-tools
